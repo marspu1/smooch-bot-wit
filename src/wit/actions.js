e0394fb403619779ceb2997495fe8cffb7fbfb09
marspu1 <ms@lifehouse.co>
2016-11-05 15:41:25 +0000

    Revert "Testing something"
    This reverts commit 2c8aab1118b51824ba8699a958ff5f1c1f89a5e7.

--- src/wit/actions.js
+++ src/wit/actions.js
@@ -28,80 +28,19 @@ const actions = {
       return resolve();
     });
   },
-  getSizeColour({context, entities}) {
+  getForecast({context, entities}) {
     return new Promise(function(resolve, reject) {
-      console.log(entities);
-      var size = firstEntityValue(entities, 'size');
-      var colour = firstEntityValue(entities, 'colour');
-
-      if (size) {
-        context.size = size;
-        delete context.missingSize;
-      }
-
-      if (!context.size && !size) {
-        context.missingSize = true;
-      }
-
-      if (colour) {
-        context.colour = colour;
-        delete context.missingColour;
-      }
-
-      if (!context.colour && !colour) {
-        context.missingColour = true;
-      }
-      // We should call retail shop api to search for clothes (Amazon API)
-      return resolve(context);
-    });
-  },
-  getLocationDateClothes({context, entities}) {
-    return new Promise(function(resolve, reject) {
-      var location = firstEntityValue(entities, 'location');
-      var datetime = firstEntityValue(entities, 'datetime');
-      var clotheType = firstEntityValue(entities, 'clotheType');
-
+      var location = firstEntityValue(entities, 'location')
       if (location) {
-        context.location = location;
+        context.forecast = 'sunny in ' + location; // we should call a weather API here
         delete context.missingLocation;
-      }
-      if (datetime) {
-        context.datetime = datetime;
-        delete context.missingDatetime;
-      }
-      if (clotheType) {
-        context.clotheType = clotheType;
-        delete context.missingClothesType;
-      }
-
-      if (!context.location && !location) {
+      } else {
         context.missingLocation = true;
-      }
-      if (!context.datetime && !datetime) {
-        context.missingDatetime = true;
-      }
-      if (!context.clotheType && !clotheType) {
-        context.missingClothesType = true;
-      }
-
-      if (location && datetime && clotheType) {
-        delete context.missingLocation;
-        delete context.missingClothesType;
-        delete context.missingDatetime;
+        delete context.forecast;
       }
       return resolve(context);
     });
   },
-  getHistoricData({context, entities})
-  {
-	  // Retrieve information the customer has supplied previously
-	  console.error('Called getHistoricData in actions.js');
-	  return new Promise(function(resolve, reject) {
-		context.BigNums = 'Big Values';
-        console.error('Called getHistoricData in actions.js');
-	    return resolve(context);
-	  });
-  }
 };
 
 module.exports = actions;
